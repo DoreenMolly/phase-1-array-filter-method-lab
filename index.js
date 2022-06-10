af5e2889141e8fd56677624b0a9f6e6f074d72c1
Doreen <wanjirudoreen13@gmail.com>
2022-06-10 18:30:27 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(from, filtered) {
+    return from.filter(
+      (theMatch) => theMatch.toLowerCase() === filtered.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(from, name) {
+    return from.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(name.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(from, filteredName) {
+    return from.filter((record) => record.name === filteredName);
+}
